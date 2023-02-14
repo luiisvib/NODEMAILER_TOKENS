738c5d13f24dc52ad3d9e7340d02a1d4a6d40aec
Luis Iñigo <luisvib2003@gmail.com>
2023-02-14 20:07:35 +0100

    Validación de tokens completada

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var  express = require("express")
 var  router = express.Router()
 var  nodemailer = require("nodemailer");
+var jwt = require("jsonwebtoken")
 
 router.get("/", (req,res) =>{
     res.render("index.ejs", {titulo: "NODE MAILER"})
@@ -9,6 +10,8 @@ router.get("/", (req,res) =>{
 router.post("/", (req,res) =>{
     const {nombre, apellidos, email} = req.body
     console.log(nombre)
+
+    const token = jwt.sign({nombre}, "my_secret_key", )
     enviar_email = async () => {
 
         let transporter = nodemailer.createTransport({
@@ -24,14 +27,27 @@ router.post("/", (req,res) =>{
             from: process.env.correo,  //Dirección de correo que enviará el email
             to: email,   //Dirección de correo que recibirá el email
             subject: "PRUEBA DAW",  
-            text: `Hola ${nombre} ${apellidos}`, 
-            html: `<h1>Hola ${nombre} ${apellidos}</h1>`, 
+            text: `Hola ${nombre} ${apellidos} aquí tienes la clave que tendrás que verificar ${token}`, 
+            html: `<h1>Hola ${nombre} ${apellidos}</h1>
+                   <p>A continuación tienes la clave que tienes que verificar</p>
+                   <h2>${token}</h2>`, 
         });
         console.log(info)
     }
     enviar_email().catch(console.error);
 
     res.render("validacion.ejs", {titulo: "Validación Token"})
+})
+
+router.post("/validacion",(req,res) =>{
+    const {token} = req.body
+    jwt.verify(token, "my_secret_key", (error, data) =>{
+        if(error){
+            res.send("<h1>TOKEN INCORRECTO</h1>")
+        }else{
+            res.send("<h1>TOKEN VALIDADO</h1>")
+        }
+    })
 })
 
 module.exports = router
